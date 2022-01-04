b65a5a2b32cde14b0239ab21228372d58c06b488
Souha <80833190+Souha001@users.noreply.github.com>
2022-01-04 17:01:08 +0100

    Add files via upload

---
+++ gestion/includes/config.php
@@ -0,0 +1,11 @@
+<?php
+/*
+
+| Author: Souhaila Rabai
+| Project Name: products management
+
+*/
+define('DB_HOST', 'localhost');          // Set database host
+define('DB_USER', 'root');             // Set database user
+define('DB_PASS', '');             // Set database password
+define('DB_NAME', 'oswa_inv');        // Set database name
